d01b27f24286bf6f0553290112ef2f3780002b29
TengHooi <chantenghooi@gmail.com>
2022-05-20 16:28:14 +0800

    shift_map: populate shift_map.launch; implement class methods

--- include/shift_map.h
+++ include/shift_map.h
@@ -5,13 +5,15 @@
 #include <vector>
 #include <string>
 
+#include <sensor_msgs/PointCloud.h>
+
 #include <ros/ros.h>
+#include <tf/transform_listener.h>
 #include <pcl_ros/transforms.h>
 
 struct NodeParameters
 {
     std::string target_frame;
-
 
 };
 
@@ -28,23 +30,33 @@ private:
 
 public:
 
-    PointCloudMapTransformer(ros::NodeHandle node);
+    PointCloudMapTransformer(ros::NodeHandle& node);
     ~PointCloudMapTransformer();
     
+    void SetNodeParameters(const ros::NodeHandle& node);
     void MapCallBack(const sensor_msgs::PointCloud::ConstPtr& input_msg);
 
 };
 
-PointCloudMapTransformer::PointCloudMapTransformer(ros::NodeHandle node)
+PointCloudMapTransformer::PointCloudMapTransformer(ros::NodeHandle& node)
 {
+    SetNodeParameters(node);
+
+    input_map_sub_ = node.subscribe<sensor_msgs::PointCloud>("input_map_cloud", 10, &PointCloudMapTransformer::MapCallBack, this);
+    output_map_pub_ = node.advertise<sensor_msgs::PointCloud>("output_map_cloud", 10);
 
 }
 
 PointCloudMapTransformer::~PointCloudMapTransformer() { }
 
-void MapCallBack(const sensor_msgs::PointCloud::ConstPtr& input_msg)
+void PointCloudMapTransformer::SetNodeParameters(const ros::NodeHandle& node)
+{   
+    node.param<std::string>("target_frame", parameters_.target_frame, "t265_odom_frame");
+}
+
+void PointCloudMapTransformer::MapCallBack(const sensor_msgs::PointCloud::ConstPtr& input_msg)
 {
-    
+
 }
 
 #endif //_SHIFT_MAP_H_
--- src/shift_map.cpp
+++ src/shift_map.cpp
@@ -1,3 +1,43 @@
+ /*
+ Copyright (c) 2022, Teng Hooi Chan, Singapore University of Technology and Design
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*************************************
+ * ROS node for transforming
+ * 
+ * sensor_msgs::PointCloud type
+ * 
+ * from original frame to other frame.
+ * 
+ * **********************************/
+
 #include "shift_map.h"
 
 int main(int argc, char** argv)
@@ -5,8 +45,10 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "shift_map");
     ros::NodeHandle node("~");
 
-    PointCloudMapTransformer map_transformer(node);
+    PointCloudMapTransformer map_transformer_node(node);
 
+    ROS_INFO("Initialized map_transformer node.");
+    
     ros::spin();
 
     return 0;
